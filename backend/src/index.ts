18378f33c97413efb4e212027784fa4cbd79f44a
Karl Bonnici <karl.bonnici100@gmail.com>
2023-03-28 11:59:47 -0700

    add /api/products endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, {Express, Request, Response} from "express";
 import { exit } from "process";
+import fs from "fs";
 
 const app: Express = express();
 const port = process.env.PORT;
@@ -13,6 +14,16 @@ const BASE_URL = '/api';
 
 app.get(`${BASE_URL}/health`, (req:Request, res:Response) => {
     res.send('Healthy!')
+})
+
+app.get(`${BASE_URL}/products`, (req: Request, res: Response) => {
+    // the path to products.json shouldn't include '../' because in the Docker 
+    // container, the current working directory is /app . Therefore,
+    // the path to products.json is ./products.json
+    let data = fs.readFileSync('./products.json').toString();
+    data = JSON.parse(data);
+    
+    res.send(data);
 })
 
 app.listen(port, ()=> {
---
+++ backend/src/product.ts
@@ -0,0 +1,9 @@
+export type Product = {
+    productId: number;
+    productName: string;
+    productOwnerName: string;
+    developers: string[];
+    scrumMasterName: string;
+    startDate: string;
+    methodology: "agile" | "waterfall";
+};
