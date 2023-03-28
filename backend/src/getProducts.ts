890c8575681229de7534aa9c181b4a2dc3bcaa8b
Karl Bonnici <karl.bonnici100@gmail.com>
2023-03-28 12:12:46 -0700

    refactor getProducts() to explicitly return Product[]

--- backend/src/getProducts.ts
+++ backend/src/getProducts.ts
@@ -1,11 +1,12 @@
 import fs from "fs";
+import { Product } from "./product";
 
-export default function getProducts() {
+export default function getProducts(): Product[] {
     // the path to products.json shouldn't include '../' because in the Docker 
     // container, the current working directory is /app . Therefore,
     // the path to products.json is ./products.json
-    let data = fs.readFileSync('./products.json').toString();
-    data = JSON.parse(data);
+    let dataString = fs.readFileSync('./products.json').toString();
+    const data: Product[] = JSON.parse(dataString);
     
     return data;
 }
