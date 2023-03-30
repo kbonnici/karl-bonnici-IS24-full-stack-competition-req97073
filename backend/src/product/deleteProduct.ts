8cf605fa8d5e4afd6c1d5d91fb73d9b1feafe62f
Karl Bonnici <karl.bonnici100@gmail.com>
2023-03-30 14:53:47 -0700

    fix bug where deleting a resource deletes everything after the resource aswell

--- backend/src/product/deleteProduct.ts
+++ backend/src/product/deleteProduct.ts
@@ -8,7 +8,7 @@ export default function deleteProduct(id: number): number | undefined {
     const productIdx = products.findIndex((p: Product) => p.productId === id);
     if (productIdx < 0) return undefined;
 
-    products.splice(productIdx);
+    products.splice(productIdx, 1);
 
     fs.writeFileSync('./products.json', JSON.stringify(products));
 
