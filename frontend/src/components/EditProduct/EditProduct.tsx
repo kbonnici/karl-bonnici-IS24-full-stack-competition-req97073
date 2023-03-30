2967f0af0618c5e13417502b9896fc9e3ee57520
Karl Bonnici <karl.bonnici100@gmail.com>
2023-03-30 14:01:43 -0700

    add call to update product

--- frontend/src/components/EditProduct/EditProduct.tsx
+++ frontend/src/components/EditProduct/EditProduct.tsx
@@ -1,6 +1,7 @@
 import { Button, Typography } from '@mui/material';
 import { Container } from '@mui/system';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Product } from '../../utils/types';
 import ControlledAlert from '../ControlledAlert/ControlledAlert';
 import Header from '../Header/Header';
@@ -22,6 +23,7 @@ function EditProduct(props: EditProductProps) {
   const [scrumMasterName, setScrumMasterName] = useState('');
   const [startDate, setStartDate] = useState('');
   const [methodology, setMethodology] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getProduct = async (id: number) => {
@@ -84,6 +86,53 @@ function EditProduct(props: EditProductProps) {
         methodology={methodology}
         setMethodology={setMethodology}
       />
+
+      <div style={{ marginTop: '10px' }}>
+        <Button
+          variant="contained"
+          style={{ marginRight: '10px' }}
+          onClick={async () => {
+            try {
+              const response = await fetch(
+                `http://localhost:80/api/product/${productId}`,
+                {
+                  method: 'PUT',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    productName,
+                    productOwnerName: productOwner,
+                    developers,
+                    startDate,
+                    scrumMasterName,
+                    methodology,
+                  }),
+                }
+              );
+
+              const json = await response.json();
+
+              if (!response.ok) {
+                setAlertText(JSON.stringify(json));
+                setAlertVisible(true);
+              } else {
+                // navigate back to landing page
+                navigate('/');
+              }
+            } catch {
+              setAlertText('Failed to save changes');
+              setAlertVisible(true);
+            }
+          }}
+        >
+          Save Changes
+        </Button>
+
+        <Button variant="outlined" href="/">
+          Go Back
+        </Button>
+      </div>
     </Container>
   );
 }
