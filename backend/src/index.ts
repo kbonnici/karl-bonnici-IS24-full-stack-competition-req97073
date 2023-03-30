96d149d7e41393dbd5225e74a9d69a0aa4ae9ddf
Karl Bonnici <karl.bonnici100@gmail.com>
2023-03-30 16:50:55 -0700

    add swagger documentation, /api-docs endpoint and fix response codes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,8 +8,26 @@ import { Product, ProductDetails } from './product/product';
 import updateProduct from './product/updateProduct';
 import { validateAndConvert } from './validateAndConvert';
 import cors from 'cors';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
-const corsOptions = {
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Express API for Product Management App',
+        version: '1.0.0',
+    },
+    servers: [{ url: 'http://localhost:80/api', description: 'API Server' }],
+};
+
+const swaggerOptions = {
+    swaggerDefinition,
+    apis: ['./src/index.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+const corsOptions: cors.CorsOptions = {
     // only allow my frontend to call this backend api
     origin: 'http://localhost:5173',
 };
@@ -18,6 +36,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 const port = process.env.PORT;
 
 if (!port) {
@@ -29,14 +48,126 @@ if (!port) {
 
 const BASE_URL = '/api';
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Errors:
+ *          type: array
+ *          items:
+ *              type: object
+ *              properties:
+ *                  minLength:
+ *                      type: string
+ *                  matches:
+ *                      type: string
+ *                  isString:
+ *                      type: string
+ *      NewProduct:
+ *          type: object
+ *          properties:
+ *              productName:
+ *                  type: string
+ *                  description: The name of the product.
+ *                  example: "myProduct"
+ *              productOwnerName:
+ *                  type: string
+ *                  description: The name of the owner of the product.
+ *                  example: "Karl"
+ *              developers:
+ *                  type: array
+ *                  items:
+ *                      type: string
+ *                  description: A list of developers working on the product.
+ *                  example: ["Karl", "Emerald"]
+ *              scrumMasterName:
+ *                  type: string
+ *                  description: The name of the scrum master for the product.
+ *                  example: "Karl"
+ *              startDate:
+ *                  type: string
+ *                  description: The Date the product started development
+ *                  example: "2023/01/31"
+ *              methodology:
+ *                  type: string
+ *                  description: The methodology the product is using. Can only be `"agile"` or `"waterfall"`.
+ *                  example: "agile"
+ *      Product:
+ *          allOf:
+ *              -   type: object
+ *                  properties:
+ *                      productId:
+ *                          type: number
+ *                          description: The Product ID.
+ *                          example: 1897
+ *              -   $ref: '#/components/schemas/NewProduct'
+ */
+
+/**
+ * @swagger
+ * /health:
+ *  get:
+ *      summary: Check API health
+ *      description: Check if API is up and running as expected.
+ *      responses:
+ *          200:
+ *              description: Whether or not the API is healthy.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              health:
+ *                                  type: string
+ *                                  description: The health of the API.
+ *                                  example: healthy!
+ */
 app.get(`${BASE_URL}/health`, (req: Request, res: Response) => {
     res.json({ health: 'healthy!' });
 });
 
+/**
+ * @swagger
+ * /products:
+ *  get:
+ *      summary: Get a list of all products
+ *      description: Returns a list of all products in the `products.json` file
+ *      responses:
+ *          200:
+ *              description: Returns a list of all products in the `products.json` file.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Product'
+ */
 app.get(`${BASE_URL}/products`, (req: Request, res: Response) => {
     res.send(getProducts());
 });
 
+/**
+ * @swagger
+ * /product/{product_id}:
+ *  get:
+ *      summary: Retrieve a single product.
+ *      description: Retrieves a single product from `products.json`.
+ *      parameters:
+ *          - in: path
+ *            name: product_id
+ *            schema:
+ *              type: number
+ *            required: true
+ *            description: Numeric ID of the product.
+ *      responses:
+ *          200:
+ *              description: A single product.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/Product'
+ */
 app.get(`${BASE_URL}/product/:product_id`, (req: Request, res: Response) => {
     const productId = parseInt(req.params.product_id);
     const product = getProduct(productId);
@@ -46,6 +177,36 @@ app.get(`${BASE_URL}/product/:product_id`, (req: Request, res: Response) => {
     else res.send(product);
 });
 
+/**
+ * @swagger
+ * /products:
+ *  post:
+ *      summary: Creates a new product
+ *      description: Creates a new product in `products.json`
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/NewProduct'
+ *      responses:
+ *          201:
+ *              description: Product created successfuly.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              productId:
+ *                                  type: number
+ *          400:
+ *              description: Bad request. Invalid request body.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          $ref: '#/components/schemas/Errors'
+ */
 app.post<{}, Product, ProductDetails, {}, {}>(
     `${BASE_URL}/products`,
     async (req: Request, res: Response) => {
@@ -61,12 +222,49 @@ app.post<{}, Product, ProductDetails, {}, {}>(
 
         const productDetails = conversionResult.data;
         const productId = createProduct(productDetails);
-        res.json({ productId });
+        res.status(201).json({ productId });
     }
 );
 
 // enable pre-flight OPTIONS request for cors to work
 app.options(`${BASE_URL}/product/:product_id`);
+/**
+ * @swagger
+ * /product/{product_id}:
+ *  put:
+ *      summary: Update a single product.
+ *      description: Updates a new product in `products.json`
+ *      parameters:
+ *          - in: path
+ *            name: product_id
+ *            schema:
+ *              type: number
+ *            required: true
+ *            description: Numeric ID of the product.
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/NewProduct'
+ *      responses:
+ *          200:
+ *              description: Product updated successfuly.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              productId:
+ *                                  type: number
+ *          400:
+ *              description: Bad request. Invalid request body.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          $ref: '#/components/schemas/Errors'
+ */
 app.put(
     `${BASE_URL}/product/:product_id`,
     async (req: Request, res: Response) => {
@@ -87,7 +285,7 @@ app.put(
         );
 
         if (productId === undefined)
-            res.status(404)
+            res.status(400)
                 .send({ errors: [`product ${productId} not found`] })
                 .end();
         else res.json({ productId });
@@ -96,12 +294,41 @@ app.put(
 
 // enable pre-flight OPTIONS request for cors to work
 app.options(`${BASE_URL}/product/:product_id`);
+/**
+ * @swagger
+ * /product/{product_id}:
+ *  delete:
+ *      summary: Delete a single product.
+ *      description: Deletes a single product from `products.json`.
+ *      parameters:
+ *          - in: path
+ *            name: product_id
+ *            schema:
+ *              type: number
+ *            required: true
+ *            description: Numeric ID of the product.
+ *      responses:
+ *          200:
+ *              description: A single product.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Invalid Request. Product not found.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/Errors'
+ */
 app.delete(`${BASE_URL}/product/:product_id`, (req: Request, res: Response) => {
     const productId = parseInt(req.params.product_id);
 
     const deletedProductId = deleteProduct(productId);
     if (deletedProductId !== productId)
-        res.status(404)
+        res.status(400)
             .send({ errors: [`product ${productId} not found`] })
             .end();
     else res.json({ productId });
